ef0bf64245633d3b9c02fc990f5f0c6cbdc68f43
Unknown <johankjellin93@gmail.com>
2018-05-17 23:46:30 +0200

    fixed with eslint, changed Movie to statles component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+/* eslint react/no-did-mount-set-state: 0 */
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
 class App extends Component {
-  
   state = {
-    movies: []
+    movies: [],
   }
 
   async componentDidMount() {
@@ -14,10 +14,10 @@ class App extends Component {
       const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=33d11acbd88e3cbb5ee457fe23524c57&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
       const movies = await res.json();
       this.setState({
-        movies: movies.results
-      })
-    } catch(e) {
-      console.log(e)
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
     }
   }
 
@@ -26,7 +26,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-        </header> 
+        </header>
         {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
