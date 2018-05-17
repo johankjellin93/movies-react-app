eb16d21b6133ccb5a42bcb0c765fa4440ef67aad
Unknown <johankjellin93@gmail.com>
2018-05-17 16:54:10 +0200

    displaying movies from array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
+
+const movies = [{
+    id: 1,
+    title: 'star wars',
+  }, {
+    id: 2,
+    title: 'iron man',
+  }, {
+    id: 3,
+    title: 'pulp fiction',
+  }
+];
 
 class App extends Component {
   render() {
@@ -9,6 +22,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header> 
+        {movies.map(movie => (
+          <Movie key={movie.id} movie={movie} /> 
+        ))}
       </div>
     );
   }
