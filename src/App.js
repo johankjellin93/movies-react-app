79d169de5b832b52e2844423fa31ad2b39497ee9
Unknown <johankjellin93@gmail.com>
2018-05-18 14:11:33 +0200

    saving doing router and switch. Added MovieList Component

--- src/App.js
+++ src/App.js
@@ -3,38 +3,33 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
+  Link,
 } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import Movie from './Movie';
 
-class App extends Component {
-  state = {
-    movies: [],
-  }
+import MoviesList from './MoviesList';
 
-  async componentDidMount() {
-    try {
-      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=33d11acbd88e3cbb5ee457fe23524c57&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
-      const movies = await res.json();
-      this.setState({
-        movies: movies.results,
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
           <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
-      </div>
-    );
-  }
-}
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/:id" component={Test} />
+      </Switch>
+    </div>
+  </Router>
+);
+
 
 export default App;
+
+const Test = ({ match }) => (
+  <h1>{match.params.id}</h1>
+);
